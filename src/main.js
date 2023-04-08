92ef7580530ec45b3578ffa8c496fb8b90dce606
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-04-08 16:28:12 +0200

    fix: Fix bugs in gameBoard Module
    - Fix board variable initialization.
    
    - Add guard pattern to getCell() and setCell() methods.
    
    - Create resetCell() method to do the resets.
    
    - Fix winner checks in checkWin() method. check whether all the cells
      are empty.
    
    - Add checkDraw() method.
    
    - Fix printBoard() method.

--- src/main.js
+++ src/main.js
@@ -26,20 +26,21 @@ const Cell = () => {
 };
 
 /**
- * GameBoard is a module that contains a game board object
- * @returns {Object} GameBoard object
+ * gameBoard is a module that contains a game board object
+ * @returns {Object} gameBoard object
  * @property {function} reset - resets the board to all empty cells
  * @property {function} getBoard - returns the board
  * @property {function} getCell - returns the cell at the given coordinates
  * @property {function} setCell - sets the cell at the given coordinates to the given value
+ * @property {function} resetCell - resets the cell at the given coordinates
  * @property {function} isFull - returns true if the board is full, false otherwise
  * @property {function} getEmptyCells - returns an array of the indices of empty cells
  * @property {function} checkWin - returns false if no winner, otherwise returns the winner
  * @property {function} printBoard - prints the board to the console
  */
-const GameBoard = (() => {
+const gameBoard = (() => {
   // board is a 3x3 array of cells
-  const board = [3][3];
+  const board = [[], [], []];
 
   /**
    * reset resets the board to all empty cells
@@ -64,7 +65,14 @@ const GameBoard = (() => {
    * @param {number} y - call it with the y coordinate
    * @returns {Object} Cell object
    */
-  const getCell = (x, y) => board[x][y];
+  const getCell = (x, y) => {
+    if (x >= 0 && x < 3 && y >= 0 && y < 3) {
+      return board[x][y];
+    }
+
+    console.error(`Invalid Coordinates (${x}, ${y})`);
+    return null;
+  };
 
   /**
    * setCell sets the cell at the given coordinates to the given value
@@ -73,7 +81,22 @@ const GameBoard = (() => {
    * @param {string} value - call it with 'X', 'O', or '_'
    */
   const setCell = (x, y, value) => {
+    if (board[x][y].getValue() !== '_') {
+      console.error(`Cell (${x}, ${y}) is already occupied`);
+      return null;
+    }
     board[x][y].setValue(value);
+
+    return board[x][y];
+  };
+
+  /**
+   * resetCell resets the cell at the given coordinates
+   * @param {number} x - call it with the x coordinate
+   * @param {number} y  - call it with the y coordinate
+   */
+  const resetCell = (x, y) => {
+    board[x][y].setValue('_');
   };
 
   /**
@@ -115,26 +138,30 @@ const GameBoard = (() => {
     for (let i = 0; i < 3; i++) {
       if (
         board[i][0].getValue() === board[i][1].getValue() &&
-        board[i][1].getValue() === board[i][2].getValue()
+        board[i][1].getValue() === board[i][2].getValue() &&
+        board[i][0].getValue() !== '_'
       ) {
         return board[i][0].getValue();
       }
       if (
         board[0][i].getValue() === board[1][i].getValue() &&
-        board[1][i].getValue() === board[2][i].getValue()
+        board[1][i].getValue() === board[2][i].getValue() &&
+        board[0][i].getValue() !== '_'
       ) {
         return board[0][i].getValue();
       }
     }
     if (
       board[0][0].getValue() === board[1][1].getValue() &&
-      board[1][1].getValue() === board[2][2].getValue()
+      board[1][1].getValue() === board[2][2].getValue() &&
+      board[0][0].getValue() !== '_'
     ) {
       return board[0][0].getValue();
     }
     if (
       board[0][2].getValue() === board[1][1].getValue() &&
-      board[1][1].getValue() === board[2][0].getValue()
+      board[1][1].getValue() === board[2][0].getValue() &&
+      board[0][2].getValue() !== '_'
     ) {
       return board[0][2].getValue();
     }
@@ -142,22 +169,33 @@ const GameBoard = (() => {
   };
 
   /**
+   * checkDraw returns true if the game is a draw, false otherwise
+   * @returns {boolean} isDraw
+   */
+  const checkDraw = () => isFull() && !checkWin();
+
+  /**
    * printBoard prints the board to the console
    */
   const printBoard = () => {
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        console.log(board[i][j].getValue());
-      }
-    }
+    board.forEach((row, rowIndex) => {
+      console.log(
+        `row${
+          rowIndex + 1
+        }: ${row[0].getValue()}  ${row[1].getValue()}  ${row[2].getValue()}`
+      );
+    });
   };
+
+  reset();
 
   return {
     reset,
     getBoard,
     getCell,
     setCell,
-    isFull,
+    resetCell,
+    checkDraw,
     getEmptyCells,
     checkWin,
     printBoard,
