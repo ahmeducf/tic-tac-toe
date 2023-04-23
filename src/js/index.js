54c4da5a386b2e8f3b03c066a980f598782fc6cb
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-04-23 22:04:58 +0200

    build: Run webpack on development mode

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ import '../index.html';
 import '../style.css';
 
 function init() {
+  window.addEventListener('load', () => {
+    document.querySelector('body').classList.remove('preload');
+  });
+
   gameController.subscribeToEvents();
   displayController.subscribeToEvents();
   displayController.init();
